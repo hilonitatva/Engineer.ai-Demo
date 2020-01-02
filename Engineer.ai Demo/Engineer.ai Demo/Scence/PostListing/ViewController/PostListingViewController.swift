387f13c33292bc7c6aac7828a3b0c5f38f12bbeb
Hiloni Sanghrajka <hiloni.sanghrajka@tatvasoft.com>
2020-01-02 14:17:25 +0530

    Initial Commit

---
+++ Engineer.ai Demo/Engineer.ai Demo/Scence/PostListing/ViewController/PostListingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostListingViewController.swift
+//  Engineer.ai Demo
+//
+//  Created by PCQ177 on 02/01/20.
+//  Copyright © 2020 PCQ177. All rights reserved.
+//
+
+import UIKit
+
+class PostListingViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Engineer.ai Demo/Engineer.ai Demo/Tools/Constant/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Engineer.ai Demo
+//
+//  Created by PCQ177 on 02/01/20.
+//  Copyright © 2020 PCQ177. All rights reserved.
+//
+
+import Foundation
